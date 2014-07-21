8d227e6afffec139a5ebf1fa65bbae05d11ae3c5
Antoine Chauvin <blackrushx@gmail.com>
2014-07-21 06:46:49 +0200

    Update app_funcs.sh

--- lib/app_funcs.sh
+++ lib/app_funcs.sh
@@ -41,7 +41,7 @@ function app_dependencies() {
   unset GIT_DIR
 
   output_section "Fetching app dependencies with mix"
-  mix deps.get --only prod || exit 1
+  mix deps.get || exit 1
 
   output_section "Compiling app dependencies"
   mix deps.compile || exit 1
